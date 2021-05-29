41370ec252a54dfaff7e7fa883d46363c0d1c5d5
Akashpm1234 <73156458+Akashpm1234@users.noreply.github.com>
2021-05-29 17:40:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+ var x, y ,z;
+
+
+ function add(){
+	 
+	 
+	 
+	 
+	
+	 
+x= Number (document.getElementById("firstfield").value) ;
+y= Number (document.getElementById("SecondField").value) ;
+	 
+	 
+	 
+	 
+	  z =x + y ;
+	 
+	 
+ alert(z);
+	 
+	 
+	  
+	 
+	 
+	 
+ }
+
+
+
